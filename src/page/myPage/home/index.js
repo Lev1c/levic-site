9fc75c3dae50d3994974391c8fda0e4a65adeedd
lev1c <dvauck2@gmail.com>
2025-03-13 18:50:02 +0400

    scroll

--- src/page/myPage/home/index.js
+++ src/page/myPage/home/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { About } from "../about";
 import { Contact } from "../contac";
 import Header from "../hedaer";
@@ -7,17 +8,75 @@ import { Portfolio } from "../portfoli";
 import { Program } from "../program";
 
 const Home = () => {
+  const sections = [
+    <Hello />,
+    <About />,
+    <Portfolio />,
+    <Program />,
+    <Contact />,
+  ];
+
+  const [currentSection, setCurrentSection] = useState(0);
+
+  const containerRef = useRef(null);
+
+  const menuItems = ["Home", "About", "Portfolio", "Program", "Contacts"];
+
+  useEffect(() => {
+    const preventScroll = (e) => {
+      e.preventDefault();
+    };
+
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("wheel", preventScroll, { passive: false });
+      container.addEventListener("touchmove", preventScroll, {
+        passive: false,
+      });
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("wheel", preventScroll);
+        container.removeEventListener("touchmove", preventScroll);
+      }
+    };
+  }, []);
+
+  const scrollToSection = (index) => {
+    setCurrentSection(index);
+  };
   return (
     <div className="">
       <Header />
       <div className="home">
-        <Hello />
-        <About />
-        <Portfolio />
-        <Program />
-        <Contact />
+        {sections.map((SectionComponent, index) => (
+          <div
+            key={index}
+            className={`section-wrapper section-wrapper-${
+              currentSection === index
+            }`}
+          >
+            {SectionComponent}
+          </div>
+        ))}
       </div>
-      <Menu />
+      <div className="menu">
+        <ul className="menu-list">
+          {menuItems.map((item, index) => (
+            <li key={index} className="menu-list_link">
+              <button
+                className={`menu-list_link_button ${
+                  currentSection === index ? "active" : ""
+                }`}
+                onClick={() => scrollToSection(index)}
+              >
+                {item}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
